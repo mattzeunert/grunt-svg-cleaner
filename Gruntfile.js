08edcf409e596d739f02af85b3d0344019675d1c
Matt Zeunert <matt@mostlystatic.com>
2014-11-01 17:48:42 +0000

    Fix svg files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,7 +56,7 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'svg_cleaner', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'svg_cleaner', 'nodeunit', 'clean']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
